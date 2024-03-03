0f11cfed1454b242a99ffca59a8b90b7993c97bc
MagicHarp <@2749380016@qq.com>
2024-03-03 11:12:51 +0800

    second

--- src/main/java/org/confluence/mod/block/ConfluenceBlocks.java
+++ src/main/java/org/confluence/mod/block/ConfluenceBlocks.java
@@ -22,6 +22,23 @@ public class ConfluenceBlocks {
     public static final RegistryObject<Block> EBONY_STONE = registerWithItem("ebony_stone", new BaseBlock());
     public static final RegistryObject<Block> EBONY_SANDSTONE = registerWithItem("ebony_sandstone", new BaseBlock());
     public static final RegistryObject<Block> EBONY_COBBLE_STONE = registerWithItem("ebony_cobble_stone", new BaseBlock());
+    public static final DecorationLogBlocks PEARL_LOG_BLOCKS = new DecorationLogBlocks("pearl");
+    public static final RegistryObject<Block> PEARL_STONE = registerWithItem("pearl_stone", new BaseBlock());
+    public static final RegistryObject<Block> PEARL_SANDSTONE = registerWithItem("pearl_sandstone", new BaseBlock());
+    public static final RegistryObject<Block> PEARL_COBBLE_STONE = registerWithItem("pearl_cobble_stone", new BaseBlock());
+    public static final DecorationLogBlocks SHADOW_LOG_BLOCKS = new DecorationLogBlocks("shadow");
+    public static final RegistryObject<Block> ANOTHER_CRIMSON_STONE = registerWithItem("another_crimson_stone", new BaseBlock());
+    public static final RegistryObject<Block> ANOTHER_CRIMSON_SANDSTONE = registerWithItem("another_crimson_sandstone", new BaseBlock());
+    public static final RegistryObject<Block> ANOTHER_CRIMSON_COBBLE_STONE = registerWithItem("another_crimson_cobble_stone", new BaseBlock());
+    public static final DecorationLogBlocks PALM_LOG_BLOCKS = new DecorationLogBlocks("palm");
+    public static final RegistryObject<Block> BIG_RUBY_BLOCK = registerWithItem("big_ruby_block", new BaseBlock());
+    public static final RegistryObject<Block> BIG_AMBER_BLOCK = registerWithItem("big_amber_block", new BaseBlock());
+    public static final RegistryObject<Block> BIG_TOPAZ_BLOCK = registerWithItem("big_topaz_block", new BaseBlock());
+    public static final RegistryObject<Block> BIG_ANOTHER_EMERALD_BLOCK = registerWithItem("big_another_emerald_block", new BaseBlock());
+    public static final RegistryObject<Block> BIG_SAPPHIRE_BLOCK = registerWithItem("big_sapphire_block", new BaseBlock());
+    public static final RegistryObject<Block> BIG_ANOTHER_AMETHYST_BLOCK = registerWithItem("big_another_amethyst_block", new BaseBlock());
+    public static final RegistryObject<Block> EMBERS_BLOCK = registerWithItem("embers_block", new BaseBlock());
+
 
     public enum Ores {
         TIN_ORE("tin_ore", new BaseBlock()),
