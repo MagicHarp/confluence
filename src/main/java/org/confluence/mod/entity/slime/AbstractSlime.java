c3b4f57f496126360960b4cfb2000a7587431c0c
westernat <westernat233@163.com>
2024-03-05 13:01:41 +0800

    assets分类

--- src/main/java/org/confluence/mod/entity/slime/AbstractSlime.java
+++ src/main/java/org/confluence/mod/entity/slime/AbstractSlime.java
@@ -4,7 +4,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.Slime;
 import net.minecraft.world.level.Level;
 
-public class AbstractSlime extends Slime {
+public abstract class AbstractSlime extends Slime {
     public AbstractSlime(EntityType<? extends Slime> p_33588_, Level p_33589_) {
         super(p_33588_, p_33589_);
     }
