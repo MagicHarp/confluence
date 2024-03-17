decc157c15efeb30ab7672ac723e7f15bbb77b4e
MagicHarp <@2749380016@qq.com>
2024-03-18 01:09:36 +0800

    调试32乘32模型

--- src/main/java/org/confluence/mod/datagen/Image32x.java
+++ src/main/java/org/confluence/mod/datagen/Image32x.java
@@ -8,11 +8,10 @@ public interface Image32x {
     default void preset(ItemModelBuilder builder) {
         builder.transforms()
             .transform(THIRD_PERSON_RIGHT_HAND).translation(0.5F, 9.75F, 0).rotation(-12, 0, 0).end()
-            .transform(FIRST_PERSON_LEFT_HAND).translation(0, 8, 6.5F).rotation(-11, 0, 0).end()
-            .transform(FIRST_PERSON_RIGHT_HAND).rotation(-74, 0, 0).end()
-            .transform(FIRST_PERSON_LEFT_HAND).rotation(-71, 0, 0).end()
-            .transform(GROUND).scale(0.5F).end()
-            .transform(GUI).translation(-2.75F, -3, 0).scale(0.45F).rotation(0, 90, 0).end()
-            .transform(FIXED).translation(3, -2.75F, 0).scale(0.5F).rotation(0, -90, 0).end();
+            .transform(FIRST_PERSON_RIGHT_HAND).rotation(-20F, -65F, 0).translation(3, 8, 0).scale(1.5F,1.5F,1F).end()
+            .transform(FIRST_PERSON_LEFT_HAND).rotation(0, 0, 0).translation(0, 8, 0).scale(0.5F,1F,1F).end()
+            .transform(GROUND).scale(1.5F,1.5F,1F).end()
+            .transform(GUI).translation(4.5F, 4.5F, 0).scale(1.5F).rotation(0, 0, 0).end()
+            .transform(FIXED).translation(0, 0, 0).scale(1.5F).rotation(0, 0, 0).end();
     }
 }
