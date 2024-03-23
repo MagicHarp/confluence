a161bb5f87f743cad01d1e18022f37c2cf2797a2
westernat <westernat233@163.com>
2024-03-23 19:04:48 +0800

    同一种饰品不能同时装备

--- src/main/java/org/confluence/mod/item/curio/BaseCurioItem.java
+++ src/main/java/org/confluence/mod/item/curio/BaseCurioItem.java
@@ -2,8 +2,12 @@ package org.confluence.mod.item.curio;
 
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Predicate;
 
 public class BaseCurioItem extends Item implements ICurioItem {
     public BaseCurioItem(Properties properties) {
@@ -16,6 +20,18 @@ public class BaseCurioItem extends Item implements ICurioItem {
 
     @Override
     public boolean canEquipFromUse(SlotContext slotContext, ItemStack stack) {
-        return true;
+        return ifEmpty(slotContext, itemStack -> itemStack.getItem() == this);
+    }
+
+    @Override
+    public boolean canEquip(SlotContext slotContext, ItemStack stack) {
+        return ifEmpty(slotContext, itemStack -> itemStack.getItem() == this);
+    }
+
+    public static boolean ifEmpty(SlotContext slotContext, Predicate<ItemStack> predicate) {
+        AtomicBoolean isEmpty = new AtomicBoolean();
+        CuriosApi.getCuriosInventory(slotContext.entity())
+            .ifPresent(handler -> isEmpty.set(handler.findCurios(predicate).isEmpty()));
+        return isEmpty.get();
     }
 }
