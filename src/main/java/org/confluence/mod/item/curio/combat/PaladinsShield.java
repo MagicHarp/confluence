d65825306a124068735b44f5fe99cd448fe88474
westernat <westernat233@163.com>
2024-04-01 13:16:53 +0800

    完善骑士盾

--- src/main/java/org/confluence/mod/item/curio/combat/PaladinsShield.java
+++ src/main/java/org/confluence/mod/item/curio/combat/PaladinsShield.java
@@ -12,10 +12,12 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.scores.Team;
 import org.confluence.mod.effect.ModEffects;
-import org.confluence.mod.effect.OwnerMobEffectInstance;
 import org.confluence.mod.item.curio.BaseCurioItem;
+import org.confluence.mod.item.curio.CurioItems;
+import org.confluence.mod.util.CuriosUtils;
 import top.theillusivec4.curios.api.SlotContext;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class PaladinsShield extends BaseCurioItem {
@@ -36,24 +38,37 @@ public class PaladinsShield extends BaseCurioItem {
         if (slotContext.entity() instanceof ServerPlayer serverPlayer && serverPlayer.level().getGameTime() % 200 == 0) {
             Team team = serverPlayer.getTeam();
             for (Player player : serverPlayer.level().players()) {
-                if (player.getTeam() != team || player.getScoreboardName().equals(serverPlayer.getScoreboardName())) continue;
-                player.addEffect(new OwnerMobEffectInstance(new MobEffectInstance(ModEffects.PALADINS_SHIELD.get(), 200), serverPlayer));
+                if (shouldSkip(player, team, serverPlayer)) continue;
+                player.addEffect(new MobEffectInstance(ModEffects.PALADINS_SHIELD.get(), 200));
             }
         }
     }
 
     public static float apply(LivingEntity living, float amount) {
         if (living instanceof ServerPlayer serverPlayer && serverPlayer.hasEffect(ModEffects.PALADINS_SHIELD.get())) {
-            MobEffectInstance instance = serverPlayer.getEffect(ModEffects.PALADINS_SHIELD.get());
-            if (instance instanceof OwnerMobEffectInstance ownerMobEffectInstance) {
-                Player player = ownerMobEffectInstance.getOwner();
-                if (player.getHealth() / player.getMaxHealth() > 0.25F) {
-                    float damage = amount * 0.25F;
-                    player.hurt(living.damageSources().playerAttack(serverPlayer), damage);
-                    return amount - damage;
+            if (CuriosUtils.hasCurio(serverPlayer, CurioItems.PALADINS_SHIELD.get())) return amount;
+            Team team = serverPlayer.getTeam();
+            ArrayList<Player> players = new ArrayList<>();
+            for (Player player : serverPlayer.level().players()) {
+                if (shouldSkip(player, team, serverPlayer)) continue;
+                if (player.getHealth() / player.getMaxHealth() > 0.25F && CuriosUtils.hasCurio(player, CurioItems.PALADINS_SHIELD.get())) {
+                    players.add(player);
                 }
             }
+            if (players.isEmpty()) return amount;
+            if (players.size() > 1) {
+                players.sort((playerA, playerB) -> (int) (playerA.distanceTo(serverPlayer) - playerB.distanceTo(serverPlayer)));
+            }
+            Player player = players.get(0);
+            float damage = amount * 0.25F;
+            player.hurt(living.damageSources().playerAttack(serverPlayer), damage);
+            return amount - damage;
+
         }
         return amount;
+    }
+
+    private static boolean shouldSkip(Player playerA, Team team, Player playerB) {
+        return playerA.getTeam() != team || playerA.getScoreboardName().equals(playerB.getScoreboardName());
     }
 }
