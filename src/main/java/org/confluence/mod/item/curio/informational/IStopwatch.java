5deb91f92b1ce6ab19ea386db35ddddc1e8dcb82
westernat <westernat233@163.com>
2024-04-05 20:42:46 +0800

    true stopwatch

--- src/main/java/org/confluence/mod/item/curio/informational/IStopwatch.java
+++ src/main/java/org/confluence/mod/item/curio/informational/IStopwatch.java
@@ -2,12 +2,17 @@ package org.confluence.mod.item.curio.informational;
 
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
+import net.minecraft.util.Mth;
 
 public interface IStopwatch {
     static Component getInfo(LocalPlayer localPlayer) {
         return Component.translatable(
             "info.confluence.stopwatch",
-            "%.2f".formatted(localPlayer.getDeltaMovement().length() * 20 - 1.568)
+            "%.2f".formatted(Mth.length(
+                localPlayer.getX() - localPlayer.xOld,
+                localPlayer.getY() - localPlayer.yOld,
+                localPlayer.getZ() - localPlayer.zOld
+            ) * 20)
         );
     }
 }
