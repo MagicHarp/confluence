cd3f3a34e7629273733350528c7de2c5ffa6f775
cooobrid <2553199163@qq.com>
2024-04-05 21:10:34 +0800

    效果完善

--- src/main/java/org/confluence/mod/effect/HarmfulEffect/SilencedEffect.java
+++ src/main/java/org/confluence/mod/effect/HarmfulEffect/SilencedEffect.java
@@ -7,11 +7,25 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.confluence.mod.item.magic.IMagicAttack;
-import org.confluence.mod.item.magic.MagicMirror;
-import org.confluence.mod.item.magic.ManaWeapons;
 
 public class SilencedEffect extends MobEffect { //沉默 禁用使用魔力的物品
     public SilencedEffect() {
         super(MobEffectCategory.HARMFUL,0xFFFAFA);
+    }
+    @SubscribeEvent
+    public void noUseMagicItem(PlayerInteractEvent.RightClickItem event) {
+        Player player = event.getEntity();
+        if (player.hasEffect(this)) {
+            if (event.getHand() == InteractionHand.MAIN_HAND) {
+                if (event.getItemStack().getItem() instanceof IMagicAttack) {
+                    event.setCanceled(true);
+                }
+            }
+        }
+    }
+    public void onAdd(Player player){
+        if (player.getMainHandItem().getItem() instanceof IMagicAttack){
+            noUseMagicItem(new PlayerInteractEvent.RightClickItem(player,InteractionHand.MAIN_HAND));
+        }
     }
 }
