fd690c8e5060cee5870ca7323d646c2737fd5f4a
MagicHarp <@2749380016@qq.com>
2024-04-15 18:52:43 +0800

    沙丘行者

--- src/main/java/org/confluence/mod/client/model/curio/DuneriderBootsModel.java
+++ src/main/java/org/confluence/mod/client/model/curio/DuneriderBootsModel.java
@@ -23,21 +23,21 @@ public class DuneriderBootsModel extends HumanoidModel<LivingEntity> {
         MeshDefinition meshdefinition = HumanoidModel.createMesh(CubeDeformation.NONE, 0.0F);
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition bone = partdefinition.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 7).addBox(0.0F, -5.0F, -2.0F, 4.0F, 5.0F, 4.0F, new CubeDeformation(0.25F))
-            .texOffs(15, 0).addBox(0.0F, -2.0F, -3.75F, 4.0F, 2.0F, 1.0F, new CubeDeformation(0.25F))
-            .texOffs(12, 7).addBox(0.0F, -2.0F, -3.0F, 4.0F, 2.0F, 1.0F, CubeDeformation.NONE)
-            .texOffs(0, 0).addBox(-0.5F, -6.0F, -2.5F, 5.0F, 2.0F, 5.0F, CubeDeformation.NONE)
-            .texOffs(11, 11).addBox(3.5F, -5.0F, 0.5F, 1.0F, 6.0F, 5.0F, new CubeDeformation(-1.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+        PartDefinition bone = partdefinition.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 7).addBox(-2F, 7.0F, -2.0F, 4.0F, 5.0F, 4.0F, new CubeDeformation(0.25F))
+            .texOffs(15, 0).addBox(-2F, 10.0F, -3.75F, 4.0F, 2.0F, 1.0F, new CubeDeformation(0.25F))
+            .texOffs(12, 7).addBox(-2F, 10.0F, -3.0F, 4.0F, 2.0F, 2.0F, CubeDeformation.NONE)
+            .texOffs(0, 0).addBox(-2.5F, 5.5F, -2.5F, 5.0F, 2.0F, 5.0F, CubeDeformation.NONE)
+            .texOffs(11, 11).addBox(2F, 6.5F, 0.5F, 1.0F, 6.0F, 5.0F, new CubeDeformation(-1.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-        bone.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(15, 3).addBox(0.5F, -2.0F, -2.25F, 3.0F, 1.0F, 1.0F, new CubeDeformation(0.25F)), PartPose.offsetAndRotation(0.0F, -1.4035F, -0.1744F, 0.4363F, 0.0F, 0.0F));
+        bone.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(15, 3).addBox(-1.5F, 9.0F, -7.25F, 3.0F, 1.0F, 1.0F, new CubeDeformation(0.25F)), PartPose.offsetAndRotation(0.0F, -1.4035F, -0.1744F, 0.4363F, 0.0F, 0.0F));
 
-        PartDefinition bone2 = partdefinition.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 7).mirror().addBox(-4.0F, -5.0F, -2.0F, 4.0F, 5.0F, 4.0F, new CubeDeformation(0.25F))
-            .texOffs(15, 0).mirror().addBox(-4.0F, -2.0F, -3.75F, 4.0F, 2.0F, 1.0F, new CubeDeformation(0.25F))
-            .texOffs(12, 7).mirror().addBox(-4.0F, -2.0F, -3.0F, 4.0F, 2.0F, 1.0F, CubeDeformation.NONE)
-            .texOffs(0, 0).mirror().addBox(-4.5F, -6.0F, -2.5F, 5.0F, 2.0F, 5.0F, CubeDeformation.NONE)
-            .texOffs(11, 11).mirror().addBox(-4.5F, -5.0F, 0.5F, 1.0F, 6.0F, 5.0F, new CubeDeformation(-1.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+        PartDefinition bone2 = partdefinition.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 7).mirror().addBox(-2.0F, 7.0F, -2.0F, 4.0F, 5.0F, 4.0F, new CubeDeformation(0.25F))
+            .texOffs(15, 0).mirror().addBox(-2.0F, 10.0F, -3.75F, 4.0F, 2.0F, 1.0F, new CubeDeformation(0.25F))
+            .texOffs(12, 7).mirror().addBox(-2.0F, 10.0F, -3.0F, 4.0F, 2.0F, 2.0F, CubeDeformation.NONE)
+            .texOffs(0, 0).mirror().addBox(-2.5F, 5.5F, -2.5F, 5.0F, 2.0F, 5.0F, CubeDeformation.NONE)
+            .texOffs(11, 11).mirror().addBox(-3F, 6.5F, 0.5F, 1.0F, 6.0F, 5.0F, new CubeDeformation(-1.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-        bone2.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(15, 3).mirror().addBox(-3.5F, -2.0F, -2.25F, 3.0F, 1.0F, 1.0F, new CubeDeformation(0.25F)), PartPose.offsetAndRotation(0.0F, -1.4035F, -0.1744F, 0.4363F, 0.0F, 0.0F));
+        bone2.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(15, 3).mirror().addBox(-1.5F, 9.0F, -7.25F, 3.0F, 1.0F, 1.0F, new CubeDeformation(0.25F)), PartPose.offsetAndRotation(0.0F, -1.4035F, -0.1744F, 0.4363F, 0.0F, 0.0F));
 
         return LayerDefinition.create(meshdefinition, 32, 32);
     }
