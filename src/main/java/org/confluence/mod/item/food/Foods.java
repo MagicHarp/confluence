47e58ddd89f43017f7225721f722c91058d69867
MagicHarp <@2749380016@qq.com>
2024-04-16 14:01:23 +0800

    改一下食物类

--- src/main/java/org/confluence/mod/item/food/Foods.java
+++ src/main/java/org/confluence/mod/item/food/Foods.java
@@ -1,30 +1,19 @@
 package org.confluence.mod.item.food;
 
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.RegistryObject;
 import org.confluence.mod.Confluence;
-import org.confluence.mod.effect.ModEffects;
 import org.confluence.mod.item.ModItems;
+import org.confluence.mod.item.common.BaseItem;
 import org.confluence.mod.util.EnumRegister;
 
 import java.util.function.Supplier;
 
 public enum Foods implements EnumRegister<Item> {
-
-    EMM("emm", (new FoodProperties.Builder()).nutrition(2).saturationMod(2.5F)
-            .effect(new MobEffectInstance(ModEffects.EXQUISITELY_STUFFED.get(), 6000, 1), 0.3F).build());
-
+    W("w", BaseItem::new),
+    ;
     private final RegistryObject<Item> value;
 
-    private static FoodProperties.Builder stew(int p_150384_) {
-        return (new FoodProperties.Builder()).nutrition(p_150384_).saturationMod(0.6F);
-    }
-    Foods(String id, FoodProperties build) {
-        RegistryObject<Item> FoodProperties = null;
-        this.value = FoodProperties;
-    }
     Foods(String id, Supplier<Item> item) {
         this.value = ModItems.ITEMS.register(id, item);
     }
@@ -37,4 +26,5 @@ public enum Foods implements EnumRegister<Item> {
     public static void init() {
         Confluence.LOGGER.info("Registering foods");
     }
+
 }
---
+++ src/main/java/org/confluence/mod/item/food/LowGradeFood.java
@@ -0,0 +1,13 @@
+package org.confluence.mod.item.food;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+
+public class LowGradeFood {
+    public static final FoodProperties E = (new FoodProperties.Builder()).nutrition(4).saturationMod(1.2F).effect(new MobEffectInstance(MobEffects.REGENERATION, 100, 1), 1.0F).effect(new MobEffectInstance(MobEffects.ABSORPTION, 2400, 0), 1.0F).alwaysEat().build();
+
+    private static FoodProperties.Builder stew(int p_150384_) {
+        return (new FoodProperties.Builder()).nutrition(p_150384_).saturationMod(0.6F);
+    }
+}
