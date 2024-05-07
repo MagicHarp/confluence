8f405207537456cf9af31abee62b561b7b1ff548
MagicHarp <@2749380016@qq.com>
2024-05-07 21:13:12 +0800

    回一手

--- src/main/java/org/confluence/mod/worldgen/biome/SurfaceRuleData.java
+++ src/main/java/org/confluence/mod/worldgen/biome/SurfaceRuleData.java
@@ -8,6 +8,7 @@ import org.confluence.mod.block.ModBlocks;
 
 public class SurfaceRuleData {
     private static final SurfaceRules.RuleSource DIRT = makeStateRule(Blocks.DIRT);
+    private static final SurfaceRules.RuleSource SAND = makeStateRule(Blocks.SAND);
     private static final SurfaceRules.RuleSource GRASS_BLOCK = makeStateRule(Blocks.GRASS_BLOCK);
     private static final SurfaceRules.RuleSource CORRUPTED_GRASS_BLOCK = makeStateRule(ModBlocks.CORRUPT_GRASS_BLOCK.get());
     private static final SurfaceRules.RuleSource EBONY_STONE = makeStateRule(ModBlocks.EBONY_STONE.get());
@@ -16,7 +17,10 @@ public class SurfaceRuleData {
 
     public static SurfaceRules.RuleSource makeRules() {
         SurfaceRules.ConditionSource isAtOrAboveWaterLevel = SurfaceRules.waterBlockCheck(-1, 0);
+        SurfaceRules.ConditionSource isUnderWaterLevel = SurfaceRules.waterStartCheck(-6, -1);
+        SurfaceRules.ConditionSource isHole = SurfaceRules.hole();
         SurfaceRules.RuleSource grassSurface = SurfaceRules.sequence(SurfaceRules.ifTrue(isAtOrAboveWaterLevel, GRASS_BLOCK), DIRT);
+        SurfaceRules.RuleSource underwaterSurface = SurfaceRules.sequence(SurfaceRules.ifTrue(isUnderWaterLevel, SAND), SAND);
         SurfaceRules.RuleSource corrupt_grassSurface = SurfaceRules.sequence(SurfaceRules.ifTrue(isAtOrAboveWaterLevel, CORRUPTED_GRASS_BLOCK), DIRT);
         SurfaceRules.RuleSource hallow_grassSurface = SurfaceRules.sequence(SurfaceRules.ifTrue(isAtOrAboveWaterLevel, HALLOW_GRASS_BLOCK), DIRT);
         SurfaceRules.RuleSource another_crimson_grassSurface = SurfaceRules.sequence(SurfaceRules.ifTrue(isAtOrAboveWaterLevel, ANOTHER_CRIMSON_GRASS_BLOCK), DIRT);
@@ -25,9 +29,6 @@ public class SurfaceRuleData {
         SurfaceRules.ifTrue(SurfaceRules.isBiome(ModBiomes.THE_CORRUPTION),
             SurfaceRules.sequence(
                     SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, corrupt_grassSurface))),
-            SurfaceRules.ifTrue(SurfaceRules.isBiome(ModBiomes.THE_CORRUPTION),
-                SurfaceRules.sequence(
-                    SurfaceRules.ifTrue(SurfaceRules.VERY_DEEP_UNDER_FLOOR, corrupt_stone_Surface))),
         SurfaceRules.ifTrue(SurfaceRules.isBiome(ModBiomes.THE_HALLOW),
             SurfaceRules.sequence(
                     SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, hallow_grassSurface))),
