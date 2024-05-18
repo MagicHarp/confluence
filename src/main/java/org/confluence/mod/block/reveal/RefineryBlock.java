6a01fd7e70091d5713234063b49d1758e9a5dafb
MagicHarp <@2749380016@qq.com>
2024-05-19 01:15:07 +0800

    提炼机の新建

---
+++ src/main/java/org/confluence/mod/block/reveal/RefineryBlock.java
@@ -0,0 +1,11 @@
+package org.confluence.mod.block.reveal;
+
+import net.minecraft.world.level.block.Block;
+
+public class RefineryBlock extends Block {
+
+
+    public RefineryBlock(Properties pProperties) {
+        super(pProperties);
+    }
+}
