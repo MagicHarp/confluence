0df60753e9892931296eab4d1fcb06167fe2b520
MagicHarp <@2749380016@qq.com>
2024-05-22 21:15:13 +0800

    微调颜色

--- src/main/java/org/confluence/mod/block/common/Torches.java
+++ src/main/java/org/confluence/mod/block/common/Torches.java
@@ -31,22 +31,22 @@ public enum Torches implements EnumRegister<Torches.ColorfulTorchBlock> {
     BLUE_TORCH("blue_torch", () -> new ColorfulTorchBlock(14.0F, 0.0F, 0.1F, 1.0F)), // 蓝
     GREEN_TORCH("green_torch", () -> new ColorfulTorchBlock(14.0F, 0.0F, 1.0F, 0.1F)), // 绿
     RED_TORCH("red_torch", () -> new ColorfulTorchBlock(14.0F, 1.0F, 0.1F, 0.1F)), // 红
-    ORANGE_TORCH("orange_torch", () -> new ColorfulTorchBlock(14.0F, 1.0F, 0.1F, 0.0F)), // 橙
+    ORANGE_TORCH("orange_torch", () -> new ColorfulTorchBlock(14.0F, 0.8F, 0.5F, 0.3F)), // 橙
     WHITE_TORCH("white_torch", () -> new ColorfulTorchBlock(14.0F, 1.0F, 1.0F, 1.0F)), // 白
-    ICE_TORCH("ice_torch", () -> new ColorfulTorchBlock(14.0F, 0.75F, 0.85F, 1.0F)), // 冰雪
+    ICE_TORCH("ice_torch", () -> new ColorfulTorchBlock(15.0F, 0.7F, 1.5F, 1.5F)), // 冰雪
     PINK_TORCH("pink_torch", () -> new ColorfulTorchBlock(14.0F, 1.0F, 0.0F, 1.0F)), // 粉
     BONE_TORCH("bone_torch", () -> new ColorfulTorchBlock(14.0F, 0.5F, 0.75F, 1.0F)), // 骨头
-    ULTRABRIGHT_TORCH("ultrabright_torch", () -> new ColorfulTorchBlock(15.0F, 0.75F, 1.0F, 1.0F)), // 超亮
+    ULTRABRIGHT_TORCH("ultrabright_torch", () -> new ColorfulTorchBlock(17.0F, 0.75F, 1.0F, 1.0F)), // 超亮
     DEMON_TORCH("demon_torch", () -> new NeedUpdate(DemonTorchColor.INSTANCE)), // 恶魔
-    CURSED_TORCH("cursed_torch", () -> new ColorfulTorchBlock(14.0F, 1.0F, 1.0F, 0.5F)), // 诅咒
+    CURSED_TORCH("cursed_torch", () -> new ColorfulTorchBlock(14.0F, 0.4F, 1.0F, 0.1F)), // 诅咒
     ICHOR_TORCH("ichor_torch", () -> new ColorfulTorchBlock(14.0F, 1.0F, 1.0F, 0.7F)), // 灵液
     RAINBOW_TORCH("rainbow_torch", () -> new NeedUpdate(RainbowTorchColor.INSTANCE)), // 彩虹
     DESERT_TORCH("desert_torch", () -> new ColorfulTorchBlock(14.0F, 1.0F, 0.85F, 0.55F)), // 沙漠
     CORAL_TORCH("coral_torch", () -> new ColorfulTorchBlock(14.0F, 0.25F, 1.0F, 0.8F)), // 珊瑚
     CORRUPT_TORCH("corrupt_torch", () -> new ColorfulTorchBlock(14.0F, 0.95F, 0.4F, 1.0F)), // 腐化
-    CRIMSON_TORCH("crimson_torch", () -> new ColorfulTorchBlock(14.0F, 1.0F, 0.7F, 0.5F)), // 猩红
+    CRIMSON_TORCH("crimson_torch", () -> new ColorfulTorchBlock(14.0F, 0.9F, 0.2F, 0.3F)), // 猩红
     HALLOWED_TORCH("hallowed_torch", () -> new ColorfulTorchBlock(14.0F, 1.0F, 0.6F, 1.0F)),// 神圣
-    JUNGLE_TORCH("jungle_torch", () -> new ColorfulTorchBlock(14.0F, -0.25F, 0.45F, -0.1F)), // 丛林
+    JUNGLE_TORCH("jungle_torch", () -> new ColorfulTorchBlock(15.0F, -0.25F, 0.45F, -0.1F)), // 丛林
     //MUSHROOM_TORCH("mushroom_torch"), // 蘑菇
     //AETHER_TORCH("aether_torch"); // 以太
     ;
