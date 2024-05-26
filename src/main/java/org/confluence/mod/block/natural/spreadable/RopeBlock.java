9b168d11fb6a5a8561ce4ae98a4e376db5f97282
MakerTechno <102357646+MakerTechno@users.noreply.github.com>
2024-05-27 00:07:08 +0800

    现在可以站在绳子上了

--- src/main/java/org/confluence/mod/block/natural/spreadable/RopeBlock.java
+++ src/main/java/org/confluence/mod/block/natural/spreadable/RopeBlock.java
@@ -9,7 +9,9 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.confluence.mod.datagen.limit.CustomItemModel;
 import org.confluence.mod.datagen.limit.CustomModel;
@@ -19,8 +21,9 @@ import static net.minecraft.world.level.block.SculkSensorBlock.WATERLOGGED;
 import static net.minecraft.world.level.block.state.properties.BlockStateProperties.BOTTOM;
 
 public class RopeBlock extends Block implements CustomModel, CustomItemModel {
+    private static final VoxelShape TOP_COLLISION = Block.box(5,15,5,11,16,11);
     public RopeBlock() {
-        super(BlockBehaviour.Properties.copy(Blocks.SCAFFOLDING));
+        super(BlockBehaviour.Properties.copy(Blocks.SCAFFOLDING).pushReaction(PushReaction.IGNORE));
     }
 
     @Override
@@ -28,7 +31,12 @@ public class RopeBlock extends Block implements CustomModel, CustomItemModel {
         return box(7, 0, 7, 9, 16, 9);
     }
 
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+    @Override
+    public @NotNull VoxelShape getCollisionShape(@NotNull BlockState pState, @NotNull BlockGetter pLevel, @NotNull BlockPos pPos, @NotNull CollisionContext pContext) {
+        return pContext.isAbove(Shapes.block(), pPos, true) && !pContext.isDescending() ? TOP_COLLISION : Shapes.empty();
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.@NotNull Builder<Block, BlockState> pBuilder) {
         pBuilder.add(WATERLOGGED, BOTTOM);
     }
 
