bc003848713e87c514d6eae9348b0fe8fb64c2da
MagicHarp <@2749380016@qq.com>
2024-05-27 18:24:51 +0800

    接口

--- src/main/java/org/confluence/mod/block/natural/BasePlantBlock.java
+++ src/main/java/org/confluence/mod/block/natural/BasePlantBlock.java
@@ -8,9 +8,10 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.FlowerBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import org.confluence.mod.datagen.limit.CustomItemModel;
 import org.confluence.mod.datagen.limit.CustomModel;
 
-public class BasePlantBlock extends FlowerBlock implements CustomModel {
+public class BasePlantBlock extends FlowerBlock implements CustomModel, CustomItemModel {
     public BasePlantBlock() {
         super(MobEffects.GLOWING,5,BlockBehaviour.Properties.copy(Blocks.DANDELION));
     }
