801421620555c53479e96f130e425fb566f81b65
westernat <westernat233@163.com>
2024-06-02 13:09:39 +0800

    some loot table2

--- src/main/java/org/confluence/mod/loot/SimpleLootModifier.java
+++ src/main/java/org/confluence/mod/loot/SimpleLootModifier.java
@@ -31,7 +31,8 @@ public class SimpleLootModifier extends LootModifier {
 
     @Override
     protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
-        context.getResolver().getLootTable(with).getRandomItems(context, generatedLoot::add);
+        if (context.getRandom().nextFloat() < 0.8F) generatedLoot.clear();
+        context.getResolver().getLootTable(with).getRandomItemsRaw(context, generatedLoot::add);
         return generatedLoot;
     }
 
