9a0d9044b545ad94845c193836de8e693ffbe716
MagicHarp <@2749380016@qq.com>
2024-06-07 23:42:03 +0800

    本体默认生命体和稀有方块配置

--- src/main/java/org/confluence/mod/misc/ModConfigs.java
+++ src/main/java/org/confluence/mod/misc/ModConfigs.java
@@ -28,12 +28,54 @@ public final class ModConfigs {
         "In order for the block to be found by the Metal Detector",
         "You need to fill the list with string like 'modid:block[state1=true]' or 'modid:block'",
         "The higher the block in the list, the higher the value"
-    ).defineListAllowEmpty("rareBlocks", List.of("minecraft:diamond_ore"), o -> true);
+    ).defineListAllowEmpty("rareBlocks", List.of(
+        "confluence:another_crimson_ore",
+        "confluence:deepslate_another_crimson_ore",
+        "confluence:ebony_ore",
+        "confluence:deepslate_ebony_ore",
+        "minecraft:ancient_debris",
+        "minecraft:diamond_ore",
+        "minecraft:deepslate_diamond_ore",
+        "confluence:platinum_ore",
+        "confluence:deepslate_platinum_ore",
+        "minecraft:gold_ore",
+        "minecraft:deepslate_gold_ore",
+        "minecraft:chest",
+        "confluence:forest_pots",
+        "confluence:tundra_pots",
+        "confluence:spider_nest_pots",
+        "confluence:underground_desert_pots",
+        "confluence:jungle_pots",
+        "confluence:marble_cave_pots",
+        "confluence:pyramid_pots",
+        "confluence:corruption_pots",
+        "confluence:another_crimson_pots",
+        "confluence:dungeon_pots",
+        "confluence:underworld_pots",
+        "confluence:lihzahrd_pots",
+        "confluence:tungsten_ore",
+        "confluence:deepslate_tungsten_ore",
+        "confluence:silver_ore",
+        "confluence:deepslate_silver_ore",
+        "confluence:lead_ore",
+        "confluence:deepslate_lead_ore",
+        "minecraft:iron_ore",
+        "minecraft:deepslate_iron_ore",
+        "confluence:tin_ore",
+        "confluence:deepslate_tin_ore",
+        "minecraft:copper_ore",
+        "minecraft:deepslate_copper_ore"
+    ), o -> true);
     private static final ForgeConfigSpec.ConfigValue<List<? extends String>> RARE_CREATURES = BUILDER.comment(
         "In order for the creature to be found by the Life Form Analyzer",
         "You need to fill the list with string like 'modid:entity'",
         "The higher the creature in the list, the higher the value"
-    ).defineListAllowEmpty("rareCreatures", List.of("minecraft:enderman"), o -> true);
+    ).defineListAllowEmpty("rareCreatures", List.of("minecraft:skeleton_horse",
+        "minecraft:sniffer",
+        "minecraft:allay",
+        "minecraft:warden",
+        "minecraft:mooshroom",
+        "minecraft:panda"), o -> true);
     public static final ForgeConfigSpec SPEC = BUILDER.build();
 
     public static Boolean dropsMoney;
