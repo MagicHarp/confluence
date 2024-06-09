84458e44f75ffc6a1d8591ce42ef5887a34646d2
westernat <westernat233@163.com>
2024-06-09 14:26:12 +0800

    断线声音

--- src/main/java/org/confluence/mod/item/curio/fishing/IHighTestFishingLine.java
+++ src/main/java/org/confluence/mod/item/curio/fishing/IHighTestFishingLine.java
@@ -1,11 +1,18 @@
 package org.confluence.mod.item.curio.fishing;
 
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.event.entity.player.ItemFishedEvent;
 import org.confluence.mod.util.CuriosUtils;
 
 public interface IHighTestFishingLine {
     static void apply(ItemFishedEvent event) {
-        if (event.getEntity().level().random.nextFloat() < 0.1429F && CuriosUtils.noSameCurio(event.getEntity(), IHighTestFishingLine.class)) {
+        Player entity = event.getEntity();
+        Level level = entity.level();
+        if (level.random.nextFloat() < 0.1429F && CuriosUtils.noSameCurio(entity, IHighTestFishingLine.class)) {
+            level.playSound(null, entity.getOnPos().above(), SoundEvents.ITEM_BREAK, SoundSource.PLAYERS);
             event.setCanceled(true);
         }
     }
