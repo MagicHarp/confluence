bdf881e61801a26fb58e610e434d9c4f8d2904ca
MagicHarp <@2749380016@qq.com>
2024-06-09 14:28:45 +0800

    调整钓鱼战利品表

--- src/main/java/org/confluence/mod/misc/ModLootTables.java
+++ src/main/java/org/confluence/mod/misc/ModLootTables.java
@@ -6,7 +6,7 @@ import org.confluence.mod.Confluence;
 public final class ModLootTables {
     public static final ResourceLocation CLAM = register("gameplay/clam");
     public static final ResourceLocation FISHING_LAVA = register("gameplay/fishing/lava");
-    public static final ResourceLocation FISH = register("gameplay/fishing/fish");
+    public static final ResourceLocation FISH = register("gameplay/fishing");
 
     private static ResourceLocation register(String id) {
         return new ResourceLocation(Confluence.MODID, id);
