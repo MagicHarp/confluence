301468046d61b77634c1753d272059269f863eb3
westernat <westernat233@163.com>
2024-06-10 13:18:47 +0800

    例子

--- src/main/java/org/confluence/mod/integration/jei/ModJeiPlugin.java
+++ src/main/java/org/confluence/mod/integration/jei/ModJeiPlugin.java
@@ -2,11 +2,16 @@ package org.confluence.mod.integration.jei;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import org.confluence.mod.Confluence;
 import org.confluence.mod.fluid.ShimmerTransmutationEvent;
+import org.confluence.mod.item.ModItems;
+import org.confluence.mod.item.curio.BaseCurioItem;
+import org.confluence.mod.item.curio.CurioItems;
 import org.jetbrains.annotations.NotNull;
 
 @JeiPlugin
@@ -26,5 +31,14 @@ public class ModJeiPlugin implements IModPlugin {
     @Override
     public void registerRecipes(@NotNull IRecipeRegistration registration) {
         registration.addRecipes(ShimmerTransmutationCategory.TYPE, ShimmerTransmutationEvent.ITEM_TRANSMUTATION);
+        for (CurioItems curio : CurioItems.values()) {
+            BaseCurioItem item = curio.get();
+            registration.addItemStackInfo(new ItemStack(item), item.getInformation());
+        }
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(ModItems.BOTTOMLESS_SHIMMER_BUCKET.get()), ShimmerTransmutationCategory.TYPE);
     }
 }
--- src/main/java/org/confluence/mod/item/curio/combat/AdhesiveBandage.java
+++ src/main/java/org/confluence/mod/item/curio/combat/AdhesiveBandage.java
@@ -1,10 +1,20 @@
 package org.confluence.mod.item.curio.combat;
 
+import net.minecraft.network.chat.Component;
 import org.confluence.mod.item.curio.BaseCurioItem;
 import org.confluence.mod.misc.ModRarity;
 
 public class AdhesiveBandage extends BaseCurioItem implements EffectInvul.Bleeding {
     public AdhesiveBandage() {
         super(ModRarity.LIGHT_RED);
+    }
+
+    @Override
+    public Component[] getInformation() {
+        return new Component[]{
+            Component.translatable("1"),
+            Component.translatable("2"),
+            Component.translatable("3")
+        };
     }
 }
