82d6ffdde788c36b83759d080114af1253f2988f
westernat <westernat233@163.com>
2024-06-12 14:08:38 +0800

    伞缓降

--- src/main/java/org/confluence/mod/item/sword/UmbrellaItem.java
+++ src/main/java/org/confluence/mod/item/sword/UmbrellaItem.java
@@ -1,13 +1,28 @@
 package org.confluence.mod.item.sword;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.level.Level;
 import org.confluence.mod.datagen.limit.CustomModel;
 import org.confluence.mod.item.ModTiers;
 import org.confluence.mod.misc.ModRarity;
+import org.jetbrains.annotations.NotNull;
 
 
-public class UmbrellaItem extends SwordItem implements  CustomModel {
+public class UmbrellaItem extends SwordItem implements CustomModel {
     public UmbrellaItem() {
         super(ModTiers.TITANIUM, 0, -2.4F, new Properties().rarity(ModRarity.BLUE));
+    }
+
+    @Override
+    public void inventoryTick(@NotNull ItemStack pStack, Level pLevel, @NotNull Entity pEntity, int pSlotId, boolean pIsSelected) {
+        if (pLevel.isClientSide) return;
+        if (pIsSelected && pEntity instanceof LivingEntity living && !living.swinging) {
+            living.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 2, 2, false, false));
+        }
     }
 }
--- src/main/java/org/confluence/mod/item/sword/ZombieArmItem.java
+++ src/main/java/org/confluence/mod/item/sword/ZombieArmItem.java
@@ -4,7 +4,7 @@ import net.minecraft.world.item.SwordItem;
 import org.confluence.mod.item.ModTiers;
 import org.confluence.mod.misc.ModRarity;
 
-public class ZombieArmItem extends SwordItem  {
+public class ZombieArmItem extends SwordItem {
     public ZombieArmItem() {
         super(ModTiers.TITANIUM, 3, -1.4F, new Properties().rarity(ModRarity.WHITE));
     }
