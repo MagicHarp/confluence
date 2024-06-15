b8db2a9e12959315c302ecf3242f7439cfaad3c4
westernat <westernat233@163.com>
2024-06-15 13:38:03 +0800

    巨石激活

--- src/main/java/org/confluence/mod/block/functional/BoulderBlock.java
+++ src/main/java/org/confluence/mod/block/functional/BoulderBlock.java
@@ -1,6 +1,7 @@
 package org.confluence.mod.block.functional;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
@@ -10,6 +11,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
+import org.confluence.mod.block.ModBlocks;
 import org.confluence.mod.datagen.limit.CustomItemModel;
 import org.confluence.mod.datagen.limit.CustomModel;
 import org.confluence.mod.entity.projectile.BoulderEntity;
@@ -36,6 +38,28 @@ public class BoulderBlock extends Block implements CustomModel, CustomItemModel 
     @Override
     public void wasExploded(@NotNull Level pLevel, @NotNull BlockPos pPos, @NotNull Explosion pExplosion) {
         summon(pLevel, pPos, entity -> pLevel.getNearestPlayer(entity, BoulderEntity.SEARCH_RANGE));
+    }
+
+    @Override
+    public boolean canConnectRedstone(BlockState state, BlockGetter level, BlockPos pos, @Nullable Direction direction) {
+        return true;
+    }
+
+    @Override
+    public void neighborChanged(@NotNull BlockState pState, Level pLevel, @NotNull BlockPos pPos, @NotNull Block pNeighborBlock, @NotNull BlockPos pNeighborPos, boolean pMovedByPiston) {
+        if (!pLevel.isClientSide) {
+            boolean should;
+            if (pLevel.hasNeighborSignal(pPos)) {
+                should = true;
+            } else {
+                BlockState below = pLevel.getBlockState(pPos.below());
+                should = below.isAir() || (below.is(ModBlocks.ACTUATORS.get()) && below.getValue(StateProperties.DRIVE));
+            }
+            if (should) {
+                pLevel.destroyBlock(pPos, false, null);
+                summon(pLevel, pPos, entity -> pLevel.getNearestPlayer(entity, BoulderEntity.SEARCH_RANGE));
+            }
+        }
     }
 
     public static void summon(Level level, BlockPos pos, Function<BoulderEntity, Player> function) {
