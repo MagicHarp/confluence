41b9e139121dc9ababe77c63d3f69b931936f3c5
westernat <westernat233@163.com>
2024-06-20 16:37:43 +0800

    移除无用的接口,交换材料位置,使用更有兼容性的方法

--- src/main/java/org/confluence/mod/item/curio/datadriven/DataDrivenCurioInfo.java
+++ src/main/java/org/confluence/mod/item/curio/datadriven/DataDrivenCurioInfo.java
@@ -12,7 +12,9 @@ import org.confluence.mod.Confluence;
 import org.confluence.mod.item.curio.ILavaImmune;
 import org.confluence.mod.item.curio.combat.*;
 import org.confluence.mod.item.curio.construction.IBreakSpeedBonus;
-import org.confluence.mod.item.curio.movement.*;
+import org.confluence.mod.item.curio.movement.IFallResistance;
+import org.confluence.mod.item.curio.movement.IJumpBoost;
+import org.confluence.mod.item.curio.movement.IMayFly;
 import top.theillusivec4.curios.Curios;
 
 import java.io.File;
@@ -45,8 +47,8 @@ public record DataDrivenCurioInfo(String id, String rarity, List<String> tooltip
         MAP.put("FallResistance", IFallResistance.class);
         MAP.put("JumpBoost", IJumpBoost.class);
         MAP.put("MayFly", IMayFly.class);
-        MAP.put("MultiJump", IMultiJump.class);
-        MAP.put("OneTimeJump", IOneTimeJump.class);
+        //MAP.put("MultiJump", IMultiJump.class);
+        //MAP.put("OneTimeJump", IOneTimeJump.class);
         MAP.put("LavaImmune", ILavaImmune.class);
         MAP.put("MagicQuiver", IMagicQuiver.class);
         MAP.put("Scope", IScope.class);
