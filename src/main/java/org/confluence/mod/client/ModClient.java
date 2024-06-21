19b834f3a0d9878cf6ed4cd5eb3d0c3676d04ebd
westernat <westernat233@163.com>
2024-06-21 17:10:25 +0800

    搞不懂

--- src/main/java/org/confluence/mod/client/ModClient.java
+++ src/main/java/org/confluence/mod/client/ModClient.java
@@ -1,18 +1,23 @@
 package org.confluence.mod.client;
 
+import com.mojang.blaze3d.platform.NativeImage;
 import net.minecraft.client.color.block.BlockColor;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.ThrownItemRenderer;
 import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.client.renderer.texture.SpriteContents;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.metadata.animation.AnimationMetadataSection;
+import net.minecraft.client.resources.metadata.animation.FrameSize;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.world.level.ColorResolver;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.client.event.RegisterColorHandlersEvent;
-import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
-import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
+import net.minecraftforge.client.event.*;
 import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
+import net.minecraftforge.client.textures.ForgeTextureMetadata;
+import net.minecraftforge.client.textures.ITextureAtlasSpriteLoader;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -47,6 +52,7 @@ import org.confluence.mod.item.common.Gels;
 import org.confluence.mod.item.curio.CurioItems;
 import org.confluence.mod.item.fishing.FishingPoles;
 import org.confluence.mod.misc.ModArmPoses;
+import org.jetbrains.annotations.NotNull;
 
 import static org.confluence.mod.Confluence.MODID;
 import static org.confluence.mod.entity.ModEntities.*;
@@ -212,5 +218,20 @@ public final class ModClient {
     @SubscribeEvent
     public static void registerBlockColors(RegisterColorHandlersEvent.Block event) {
         event.register(HALLOW_LEAVES_COLOR, ModBlocks.PEARL_LOG_BLOCKS.LEAVES.get());
+    }
+
+    @SubscribeEvent
+    public static void registerTextureAtlasSpriteLoaders(RegisterTextureAtlasSpriteLoadersEvent event) {
+        event.register("still_fluid", new ITextureAtlasSpriteLoader() {
+            @Override
+            public SpriteContents loadContents(ResourceLocation name, Resource resource, FrameSize frameSize, NativeImage image, AnimationMetadataSection animationMeta, ForgeTextureMetadata forgeMeta) {
+                return new SpriteContents(name, frameSize, image, animationMeta, forgeMeta);
+            }
+
+            @Override
+            public @NotNull TextureAtlasSprite makeSprite(ResourceLocation atlasName, SpriteContents contents, int atlasWidth, int atlasHeight, int spriteX, int spriteY, int mipmapLevel) {
+                return new TextureAtlasSprite(atlasName, contents, 16, 512, 0, 0) {};
+            }
+        });
     }
 }
