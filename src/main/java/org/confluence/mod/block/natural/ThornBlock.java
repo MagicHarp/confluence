f29362f774ae92062dca56e81693513a69a5a777
westernat <westernat233@163.com>
2024-06-29 21:02:01 +0800

    修复荆棘连接问题

--- src/main/java/org/confluence/mod/block/natural/ThornBlock.java
+++ src/main/java/org/confluence/mod/block/natural/ThornBlock.java
@@ -7,6 +7,8 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
@@ -20,57 +22,72 @@ import net.minecraft.world.level.material.PushReaction;
 import org.confluence.mod.datagen.limit.CustomModel;
 import org.confluence.mod.misc.ModDamageTypes;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class ThornBlock extends PipeBlock implements CustomModel {
     private final float amount;
     private static final IntegerProperty PROP_AGE = IntegerProperty.create("age", 0, 7);
 
-    public ThornBlock(float amount){
+    public ThornBlock(float amount) {
         super(0.3125f, BlockBehaviour.Properties.of().replaceable().noCollission().instabreak().sound(SoundType.GRASS).pushReaction(PushReaction.DESTROY).randomTicks());
         this.amount = amount;
         this.registerDefaultState(this.stateDefinition.any().setValue(NORTH, false).setValue(EAST, false).setValue(SOUTH, false).setValue(WEST, false).setValue(UP, false).setValue(DOWN, false));
     }
 
+    @Nullable
     @Override
-    public void entityInside(@NotNull BlockState pState, @NotNull Level pLevel, @NotNull BlockPos pPos, @NotNull Entity pEntity){
-        if(pLevel.isClientSide())return;
-        if(pEntity instanceof ServerPlayer player){
-            player.hurt(ModDamageTypes.of(pLevel,ModDamageTypes.THORN),amount);
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return getStateForPlacement(pContext.getLevel(), pContext.getClickedPos());
+    }
+
+    public BlockState getStateForPlacement(BlockGetter pLevel, BlockPos pPos) {
+        BlockState blockState = defaultBlockState();
+        for (Direction direction : Direction.values()) {
+            blockState.setValue(PROPERTY_BY_DIRECTION.get(direction), pLevel.getBlockState(pPos.offset(direction.getNormal())).is(this));
+        }
+        return blockState;
+    }
+
+    @Override
+    public void entityInside(@NotNull BlockState pState, @NotNull Level pLevel, @NotNull BlockPos pPos, @NotNull Entity pEntity) {
+        if (pLevel.isClientSide()) return;
+        if (pEntity instanceof ServerPlayer player) {
+            player.hurt(ModDamageTypes.of(pLevel, ModDamageTypes.THORN), amount);
             pLevel.destroyBlock(pPos, false);
         }
-        if(pEntity instanceof Projectile){
+        if (pEntity instanceof Projectile) {
             pLevel.destroyBlock(pPos, false);
         }
     }
 
     @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder){
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(NORTH, EAST, SOUTH, WEST, UP, DOWN, PROP_AGE);
         super.createBlockStateDefinition(pBuilder);
     }
 
     @Override
-    public boolean isRandomlyTicking(@NotNull BlockState pState){
+    public boolean isRandomlyTicking(@NotNull BlockState pState) {
         return pState.getValue(PROP_AGE) < 7;
     }
 
     @Override
-    public void randomTick(@NotNull BlockState pState, @NotNull ServerLevel pLevel, @NotNull BlockPos pPos, @NotNull RandomSource pRandom){
+    public void randomTick(@NotNull BlockState pState, @NotNull ServerLevel pLevel, @NotNull BlockPos pPos, @NotNull RandomSource pRandom) {
         // TODO: 降低概率
         Direction dir = Direction.getRandom(pRandom);
         BlockPos targetPos = pPos.relative(dir);
-        if(!pLevel.getBlockState(targetPos).isAir() || checkBorder(pLevel,targetPos,dir))return;
+        if (!pLevel.getBlockState(targetPos).isAir() || checkBorder(pLevel, targetPos, dir)) return;
 
 
         int age = pState.getValue(PROP_AGE);
-        pLevel.setBlockAndUpdate(targetPos, defaultBlockState().setValue(PROP_AGE, Math.min(7, age + pRandom.nextInt(1, 3))));
-        pLevel.setBlockAndUpdate(pPos, defaultBlockState().setValue(PROP_AGE, pRandom.nextBoolean() ? 7 : age + 1));
+        pLevel.setBlock(targetPos, getStateForPlacement(pLevel, targetPos).setValue(PROP_AGE, Math.min(7, age + pRandom.nextInt(1, 3))), Block.UPDATE_CLIENTS);
+        pLevel.setBlock(pPos, pState.setValue(PROPERTY_BY_DIRECTION.get(dir), true).setValue(PROP_AGE, pRandom.nextBoolean() ? 7 : age + 1), Block.UPDATE_CLIENTS);
     }
 
-    private boolean checkBorder(Level level, BlockPos pos, Direction direction){
-        for(Direction value : Direction.values()){
-            if(value==direction || value==direction.getOpposite())continue;
-            if(level.getBlockState(pos.relative(value)).is(this)){
+    private boolean checkBorder(Level level, BlockPos pos, Direction direction) {
+        for (Direction value : Direction.values()) {
+            if (value == direction || value == direction.getOpposite()) continue;
+            if (level.getBlockState(pos.relative(value)).is(this)) {
                 return true;
             }
         }
@@ -79,7 +96,7 @@ public class ThornBlock extends PipeBlock implements CustomModel {
 
     @Override
     @NotNull
-    public BlockState updateShape(@NotNull BlockState pState, @NotNull Direction pDirection, @NotNull BlockState pNeighborState, @NotNull LevelAccessor pLevel, @NotNull BlockPos pPos, @NotNull BlockPos pNeighborPos){
+    public BlockState updateShape(@NotNull BlockState pState, @NotNull Direction pDirection, @NotNull BlockState pNeighborState, @NotNull LevelAccessor pLevel, @NotNull BlockPos pPos, @NotNull BlockPos pNeighborPos) {
         return pState.setValue(PROPERTY_BY_DIRECTION.get(pDirection), pNeighborState.is(this));
     }
 }
