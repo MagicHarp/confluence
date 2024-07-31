ac823684fb225907e6c84f8820280d58dd384ea6
cooobrid <2553199163@qq.com>
2024-07-31 17:22:04 +0800

    先这样

--- src/main/java/org/confluence/mod/block/natural/CoinPileBlock.java
+++ src/main/java/org/confluence/mod/block/natural/CoinPileBlock.java
@@ -8,7 +8,7 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.FallingBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
@@ -20,35 +20,42 @@ import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.confluence.mod.datagen.limit.CustomItemModel;
 import org.confluence.mod.datagen.limit.CustomModel;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
+import java.util.EnumMap;
+import java.util.Map;
 
-public class CoinPileBlock extends Block implements CustomModel, CustomItemModel {
+public class CoinPileBlock extends FallingBlock implements CustomModel, CustomItemModel {
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-    private static final IntegerProperty HEAPS = IntegerProperty.create("heaps", 1, 3);
-    private static final VoxelShape DEFAULT_AABB = Block.box(6.0, 0.0, 6.0, 10.0, 6.0, 10.0);
-    private static final VoxelShape TWO_AABB = Block.box(3.0, 0.0, 3.0, 13.0, 6.0, 13.0);
-    private static final VoxelShape THREE_AABB = Block.box(2.0, 0.0, 2.0, 14.0, 6.0, 14.0);
+    public static final BooleanProperty NORTH = BooleanProperty.create("north");
+    private static final IntegerProperty HEAPS = IntegerProperty.create("heaps", 1, 13);
+    private static final Map<Direction, BooleanProperty> PROPERTY_BY_DIRECTION = new EnumMap<>(Direction.class);
 
+    static {
+        PROPERTY_BY_DIRECTION.put(Direction.NORTH, NORTH);
+    }
 
     public CoinPileBlock() {
-        super(BlockBehaviour.Properties.of().sound(SoundType.AMETHYST));
-        registerDefaultState(this.defaultBlockState().setValue(HEAPS, 1).setValue(WATERLOGGED, true));
+        super(Properties.of().sound(SoundType.AMETHYST));
+        registerDefaultState(this.defaultBlockState().setValue(HEAPS, 1).setValue(WATERLOGGED, true).setValue(NORTH, false));
     }
 
     @Override
     public @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter world, @NotNull BlockPos pos, @NotNull CollisionContext context) {
         int heaps = state.getValue(HEAPS);
-        if (heaps == 1){
-            return DEFAULT_AABB;
-        }else if (heaps == 2){
-            return TWO_AABB;
+        if (heaps <= 3){
+           return Block.box(6.0, 0.0, 6.0, 10.0, 6.0, 10.0);
+        }else if(heaps <= 6){
+            return Block.box(3.0, 0.0, 3.0, 13.0, 6.0, 13.0);
+        }else if (heaps <= 9){
+            return Block.box(2.0, 0.0, 2.0, 14.0, 6.0, 14.0);
         }else {
-            return THREE_AABB;
+            return Block.box(2.0, 0.0, 2.0, 14.0, 7.0, 14.0);
         }
     }
 
@@ -59,35 +66,49 @@ public class CoinPileBlock extends Block implements CustomModel, CustomItemModel
 
     @Override
     public boolean canBeReplaced(BlockState state, BlockPlaceContext UseContext) {
-        return !UseContext.isSecondaryUseActive() && UseContext.getItemInHand().is(this.asItem()) && state.getValue(HEAPS) < 3 || super.canBeReplaced(state, UseContext);
+        return !UseContext.isSecondaryUseActive() && UseContext.getItemInHand().is(this.asItem()) && state.getValue(HEAPS) < 13 || super.canBeReplaced(state, UseContext);
     }
+
 
     @Nullable
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        BlockState placeState = context.getLevel().getBlockState(context.getClickedPos());
-        if (placeState.is(this)) {
-            return placeState.setValue(HEAPS, Math.min(3, placeState.getValue(HEAPS) + 1));
-        } else {
-            FluidState fluidState = context.getLevel().getFluidState(context.getClickedPos());
-            boolean isWater = fluidState.getType() == Fluids.WATER;
-            return super.getStateForPlacement(context).setValue(WATERLOGGED, isWater);
+        BlockState clickedBlockState = context.getLevel().getBlockState(context.getClickedPos());
+        if (clickedBlockState.is(this)) {
+            return clickedBlockState.setValue(HEAPS, Math.min(13, clickedBlockState.getValue(HEAPS) + 1));
         }
+
+        BlockState clickedBlockBelowState = context.getLevel().getBlockState(context.getClickedPos().below());
+        if (clickedBlockBelowState.is(this)) {
+            BlockState state = this.defaultBlockState();
+            state = state.setValue(PROPERTY_BY_DIRECTION.get(Direction.NORTH), true);
+
+            FluidState fluidState = context.getLevel().getFluidState(context.getClickedPos());
+            if (fluidState.getType() == Fluids.WATER) {
+                state = state.setValue(WATERLOGGED, true);
+            }
+
+            return state;
+        }
+        return this.defaultBlockState();
     }
 
-    @Override
-    public boolean canSurvive(@NotNull BlockState state, LevelReader level, BlockPos pos) {
-        BlockState Below = level.getBlockState(pos.below());
-        return Below.is(this) || !Below.isAir();
-    }
+        @Override
+        public boolean canSurvive (@NotNull BlockState state, LevelReader level, BlockPos pos){
+            BlockState Below = level.getBlockState(pos.below());
+            return Below.is(this) || !Below.isAir();
+        }
 
-    @Override
-    public BlockState updateShape(BlockState state, @NotNull Direction facing, @NotNull BlockState facingState, @NotNull LevelAccessor level, @NotNull BlockPos CurrentPos, @NotNull BlockPos facingPos) {
-        return !state.canSurvive(level, CurrentPos) ? Blocks.AIR.defaultBlockState() : super.updateShape(state, facing, facingState, level, CurrentPos, facingPos);
-    }
+        @Override
+        public @NotNull BlockState updateShape (BlockState state, @NotNull Direction facing, @NotNull BlockState
+        facingState, @NotNull LevelAccessor level, @NotNull BlockPos CurrentPos, @NotNull BlockPos facingPos){
+            if (state.getValue(WATERLOGGED)) {
+                level.scheduleTick(CurrentPos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+            }
+            return super.updateShape(state, facing, facingState, level, CurrentPos, facingPos);
+        }
 
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(HEAPS);
-        builder.add(WATERLOGGED);
-    }
+        @Override
+        protected void createBlockStateDefinition (StateDefinition.Builder < Block, BlockState > builder){
+            builder.add(HEAPS, WATERLOGGED, NORTH);
+        }
 }
