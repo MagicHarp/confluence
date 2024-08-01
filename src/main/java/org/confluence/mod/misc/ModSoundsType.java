d473c35d5b1d50818bdb2ac5d66fcf43e596ef43
cooobrid <2553199163@qq.com>
2024-08-01 15:55:03 +0800

    忘了格式化了

--- src/main/java/org/confluence/mod/misc/ModSoundsType.java
+++ src/main/java/org/confluence/mod/misc/ModSoundsType.java
@@ -3,5 +3,6 @@ package org.confluence.mod.misc;
 import net.minecraft.world.level.block.SoundType;
 
 public class ModSoundsType {
+
     public static final SoundType COIN = new SoundType(1.0F, 1.0F, ModSoundsEvent.COINS.get(), ModSoundsEvent.COINS.get(), ModSoundsEvent.COINS.get(), ModSoundsEvent.COINS.get(), ModSoundsEvent.COINS.get());
 }
