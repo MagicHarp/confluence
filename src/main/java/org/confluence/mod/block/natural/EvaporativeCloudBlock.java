56c568527891ce74f1cd66e54d8e099c85382a52
cooobrid <2553199163@qq.com>
2024-08-02 22:36:49 +0800

    先这样吧 有问题再说

--- src/main/java/org/confluence/mod/block/natural/EvaporativeCloudBlock.java
+++ src/main/java/org/confluence/mod/block/natural/EvaporativeCloudBlock.java
@@ -1,6 +1,7 @@
 package org.confluence.mod.block.natural;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
@@ -15,20 +16,22 @@ import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import org.jetbrains.annotations.NotNull;
 
 public class EvaporativeCloudBlock extends Block {
-    private static final IntegerProperty SURVIVE_TIME = IntegerProperty.create("survive_time", 0, 1);
+    private static final IntegerProperty SURVIVE_TIME = IntegerProperty.create("survive_time", 0, 10);
 
     public EvaporativeCloudBlock() {
         super(BlockBehaviour.Properties.copy(Blocks.GLASS).sound(SoundType.SNOW).randomTicks());
-        this.registerDefaultState(this.defaultBlockState().setValue(SURVIVE_TIME, 0));
+        registerDefaultState(this.defaultBlockState().setValue(SURVIVE_TIME, 0));
     }
 
     @Override
-    public void randomTick(BlockState state, @NotNull ServerLevel level, @NotNull BlockPos pos, @NotNull RandomSource random) {
-        int surviveTime = state.getValue(SURVIVE_TIME);
-        if (surviveTime < 1) {
-            level.setBlock(pos, state.setValue(SURVIVE_TIME, surviveTime + 1), Block.UPDATE_CLIENTS);
-        } else if (surviveTime == 1) {
-            level.removeBlock(pos, false);
+    public void tick(BlockState state, @NotNull ServerLevel level, @NotNull BlockPos pos, @NotNull RandomSource random) {
+        for (Direction direction : Direction.values()) {
+            BlockPos neighborPos = pos.relative(direction);
+            BlockState neighborState = level.getBlockState(neighborPos);
+
+            if (neighborState.getBlock() == Blocks.WATER) {
+                level.setBlockAndUpdate(neighborPos, Blocks.WATER.defaultBlockState());
+            }
         }
     }
 
