e3ddda6e9a86b3569744f3043cb36c580e7bb400
cooobrid <2553199163@qq.com>
2024-08-03 10:33:08 +0800

    先这样 没法实时更新

--- src/main/java/org/confluence/mod/block/natural/EvaporativeCloudBlock.java
+++ src/main/java/org/confluence/mod/block/natural/EvaporativeCloudBlock.java
@@ -12,25 +12,27 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import org.jetbrains.annotations.NotNull;
 
 public class EvaporativeCloudBlock extends Block {
-    private static final IntegerProperty SURVIVE_TIME = IntegerProperty.create("survive_time", 0, 10);
+    private static final BooleanProperty NeighborBlockState = BooleanProperty.create("neighbor_block_state");
 
     public EvaporativeCloudBlock() {
         super(BlockBehaviour.Properties.copy(Blocks.GLASS).sound(SoundType.SNOW).randomTicks());
-        registerDefaultState(this.defaultBlockState().setValue(SURVIVE_TIME, 0));
+        registerDefaultState(this.defaultBlockState().setValue(NeighborBlockState, false));
     }
 
     @Override
-    public void tick(BlockState state, @NotNull ServerLevel level, @NotNull BlockPos pos, @NotNull RandomSource random) {
+    public void tick(@NotNull BlockState state, @NotNull ServerLevel level, @NotNull BlockPos pos, @NotNull RandomSource random) {
         for (Direction direction : Direction.values()) {
             BlockPos neighborPos = pos.relative(direction);
             BlockState neighborState = level.getBlockState(neighborPos);
 
             if (neighborState.getBlock() == Blocks.WATER) {
                 level.setBlockAndUpdate(neighborPos, Blocks.WATER.defaultBlockState());
+                state =state.setValue(NeighborBlockState,true);
+                level.setBlockAndUpdate(pos,state);
             }
         }
     }
@@ -43,6 +45,6 @@ public class EvaporativeCloudBlock extends Block {
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(SURVIVE_TIME);
+        builder.add(NeighborBlockState);
     }
 }
