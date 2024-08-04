cd9de70a211a3f7d0527fbf263bb4d2f2c0bd5b8
cooobrid <2553199163@qq.com>
2024-08-04 12:58:24 +0800

    格式化

--- src/main/java/org/confluence/mod/item/common/ThrowingKnivesItem.java
+++ src/main/java/org/confluence/mod/item/common/ThrowingKnivesItem.java
@@ -11,26 +11,26 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
-    public class ThrowingKnivesItem extends Item {
-        public ThrowingKnivesItem() {
-            super(new Properties());
-        }
-
-        public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
-            ItemStack itemstack = pPlayer.getItemInHand(pHand);
-            pLevel.playSound((Player)null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.ARROW_SHOOT, SoundSource.NEUTRAL, 0.5F, 0.4F / (pLevel.getRandom().nextFloat() * 0.4F + 0.8F));
-            if (!pLevel.isClientSide) {
-                Snowball snowball = new Snowball(pLevel, pPlayer);
-                snowball.setItem(itemstack);
-                snowball.shootFromRotation(pPlayer, pPlayer.getXRot(), pPlayer.getYRot(), 0.0F, 1.5F, 1.0F);
-                pLevel.addFreshEntity(snowball);
-            }
-
-            pPlayer.awardStat(Stats.ITEM_USED.get(this));
-            if (!pPlayer.getAbilities().instabuild) {
-                itemstack.shrink(1);
-            }
-
-            return InteractionResultHolder.sidedSuccess(itemstack, pLevel.isClientSide());
-        }
+public class ThrowingKnivesItem extends Item {
+    public ThrowingKnivesItem() {
+        super(new Properties());
     }
+
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
+        ItemStack itemstack = pPlayer.getItemInHand(pHand);
+        pLevel.playSound(null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.ARROW_SHOOT, SoundSource.NEUTRAL, 0.5F, 0.4F / (pLevel.getRandom().nextFloat() * 0.4F + 0.8F));
+        if (!pLevel.isClientSide) {
+            Snowball snowball = new Snowball(pLevel, pPlayer);
+            snowball.setItem(itemstack);
+            snowball.shootFromRotation(pPlayer, pPlayer.getXRot(), pPlayer.getYRot(), 0.0F, 1.5F, 1.0F);
+            pLevel.addFreshEntity(snowball);
+        }
+
+        pPlayer.awardStat(Stats.ITEM_USED.get(this));
+        if (!pPlayer.getAbilities().instabuild) {
+            itemstack.shrink(1);
+        }
+
+        return InteractionResultHolder.sidedSuccess(itemstack, pLevel.isClientSide());
+    }
+}
