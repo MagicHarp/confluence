125dd517e463a4cedc93d4145012684885267aee
MagicHarp <@2749380016@qq.com>
2024-08-05 14:18:32 +0800

    雪泥块材质

--- src/main/java/org/confluence/mod/block/natural/SlushBlock.java
+++ src/main/java/org/confluence/mod/block/natural/SlushBlock.java
@@ -4,10 +4,10 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.FallingBlock;
 import net.minecraft.world.level.block.SoundType;
-import org.confluence.mod.datagen.limit.CustomModel;
+import org.confluence.mod.block.natural.spreadable.ISpreadable;
 
-public class SlushBlock extends FallingBlock implements CustomModel {
-    public SlushBlock() {
+public class SlushBlock extends FallingBlock  {
+    public SlushBlock(int color) {
         super(Properties.copy(Blocks.GRAVEL)
             .strength(1.0F, 1.0F)
             .sound(SoundType.GRAVEL)
