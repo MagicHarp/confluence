6f50d7a45a3f51a72f7de9d04c636a8f6bcb6dd5
voila <voila1106@gmail.com>
2024-08-09 23:36:53 +0800

    恶魔眼

--- src/main/java/org/confluence/mod/client/renderer/entity/DemonEyeRenderer.java
+++ src/main/java/org/confluence/mod/client/renderer/entity/DemonEyeRenderer.java
@@ -1,6 +1,9 @@
 package org.confluence.mod.client.renderer.entity;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.util.Mth;
 import org.confluence.mod.client.model.entity.DemonEyeModel;
 import org.confluence.mod.entity.demoneye.DemonEye;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
@@ -8,5 +11,11 @@ import software.bernie.geckolib.renderer.GeoEntityRenderer;
 public class DemonEyeRenderer extends GeoEntityRenderer<DemonEye> {
     public DemonEyeRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new DemonEyeModel());
+    }
+
+    @Override
+    protected void applyRotations(DemonEye animatable, PoseStack poseStack, float ageInTicks, float rotationYaw, float partialTick){
+        super.applyRotations(animatable, poseStack, ageInTicks, rotationYaw, partialTick);
+        poseStack.mulPose(Axis.XP.rotationDegrees(Mth.lerp(partialTick,animatable.xRotO,animatable.getXRot())));
     }
 }
