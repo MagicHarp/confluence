3d4787032d79578601dad0f15b54c9502b2acf48
MagicHarp <@2749380016@qq.com>
2024-08-10 14:25:32 +0800

    恶魔眼数值对半砍

--- src/main/java/org/confluence/mod/entity/demoneye/DemonEyeVariant.java
+++ src/main/java/org/confluence/mod/entity/demoneye/DemonEyeVariant.java
@@ -9,21 +9,21 @@ import org.jetbrains.annotations.NotNull;
 import java.util.function.IntFunction;
 
 public enum DemonEyeVariant implements StringRepresentable {
-    NORMAL(0, "normal", false, 30.0, 9.0, 2),
-    NORMAL_BIG(1, "normal_big", true, 34.5, 10.0, 4),
-    CATARACT(2, "cataract", false, 32.5, 9.0, 4),
-    CATARACT_BIG(3, "cataract_big", true, 37.0, 10.0, 4),
-    SLEEPY(4, "sleepy", false, 30.0, 8.0, 2),
-    SLEEPY_BIG(5, "sleepy_big", true, 33.0, 8.5, 2),
-    DILATED(6, "dilated", true, 25.0, 9.0, 2),
-    DILATED_SMALL(7, "dilated_small", false, 22.5, 8.0, 1),
-    GREEN(8, "green", true, 30.0, 10.0, 0),
-    GREEN_SMALL(9, "green_small", false, 25.5, 8.5, 0),
-    PURPLE(10, "purple", false, 30.0, 7.0, 4),
-    PURPLE_BIG(11, "purple_big", true, 33.0, 7.5, 4),
+    NORMAL(0, "normal", false, 15.0, 4.5, 1),
+    NORMAL_BIG(1, "normal_big", true, 12.0, 5.0, 2),
+    CATARACT(2, "cataract", false, 11.5, 4.5, 2),
+    CATARACT_BIG(3, "cataract_big", true, 14.0, 5.0, 2),
+    SLEEPY(4, "sleepy", false, 15.0, 4.0, 1),
+    SLEEPY_BIG(5, "sleepy_big", true, 16.0, 4.5, 1),
+    DILATED(6, "dilated", true, 12.0, 4.5, 1),
+    DILATED_SMALL(7, "dilated_small", false, 11.5, 4.0, 0),
+    GREEN(8, "green", true, 15.0, 5.0, 0),
+    GREEN_SMALL(9, "green_small", false, 12.5, 4.0, 0),
+    PURPLE(10, "purple", false, 15.0, 3.0, 2),
+    PURPLE_BIG(11, "purple_big", true, 16.0, 3.0, 2),
 
-    OWL(12, "owl", false, 37.5, 8.0, 6),
-    SPACESHIP(13, "spaceship", false, 30.0, 10.0, 4);
+    OWL(12, "owl", false, 18.5, 4.0, 3),
+    SPACESHIP(13, "spaceship", false, 15.0, 5.0, 2);
 
     private static final IntFunction<DemonEyeVariant> BY_ID = ByIdMap.continuous(DemonEyeVariant::getId, values(), ByIdMap.OutOfBoundsStrategy.CLAMP);
     final int id;
