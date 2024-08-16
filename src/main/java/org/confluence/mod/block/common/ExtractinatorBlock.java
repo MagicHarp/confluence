b7d243f55bf868832fa4178a5bf605d73419fd02
MagicHarp <@2749380016@qq.com>
2024-08-16 20:43:42 +0800

    肘击失败，救一手（

--- src/main/java/org/confluence/mod/block/common/ExtractinatorBlock.java
+++ src/main/java/org/confluence/mod/block/common/ExtractinatorBlock.java
@@ -1,31 +1,69 @@
 package org.confluence.mod.block.common;
 
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import org.confluence.mod.block.ModBlocks;
+import org.confluence.mod.client.model.block.ExtractinatorBlockModel;
+import org.confluence.mod.client.model.block.LifeCrystalBlockModel;
+import org.confluence.mod.datagen.limit.CustomItemModel;
+import org.confluence.mod.datagen.limit.CustomModel;
 import org.confluence.mod.misc.ModLootTables;
+import org.confluence.mod.misc.ModRarity;
 import org.confluence.mod.misc.ModTags;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.animatable.GeoBlockEntity;
+import software.bernie.geckolib.animatable.GeoItem;
+import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.AnimatableManager;
+import software.bernie.geckolib.model.GeoModel;
+import software.bernie.geckolib.renderer.GeoItemRenderer;
+import software.bernie.geckolib.util.GeckoLibUtil;
 
-public class ExtractinatorBlock extends Block {
+import java.util.function.Consumer;
+
+public class ExtractinatorBlock extends HorizontalDirectionalBlock implements EntityBlock, CustomModel, CustomItemModel {
     public ExtractinatorBlock(Properties pProperties) {
         super(pProperties);
     }
 
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING);
+    }
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(@NotNull BlockPos pPos, @NotNull BlockState pState) {
+        return new LifeCrystalBlock.Entity(pPos, pState);
+    }
     @Override
     public @NotNull RenderShape getRenderShape(@NotNull BlockState state) {
         return RenderShape.ENTITYBLOCK_ANIMATED;
@@ -64,5 +102,65 @@ public class ExtractinatorBlock extends Block {
             pPlayer.getItemInHand(pHand).setCount(item.getCount() - 1);
         }
         return InteractionResult.SUCCESS;
+    }
+    public static class Entity extends BlockEntity implements GeoBlockEntity {
+        private final AnimatableInstanceCache CACHE = GeckoLibUtil.createInstanceCache(this);
+
+        public Entity(BlockPos pPos, BlockState pBlockState) {
+            super(ModBlocks.EXTRACTINATOR_ENTITY.get(), pPos, pBlockState);
+        }
+
+        @Override
+        public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {}
+
+        @Override
+        public AnimatableInstanceCache getAnimatableInstanceCache() {
+            return CACHE;
+        }
+    }
+    public static class Item extends BlockItem implements GeoItem {
+        private final AnimatableInstanceCache CACHE = GeckoLibUtil.createInstanceCache(this);
+
+        public Item(ExtractinatorBlock pBlock) {
+            super(pBlock, new Properties().rarity(ModRarity.WHITE));
+        }
+
+        @Override
+        public void initializeClient(Consumer<IClientItemExtensions> consumer) {
+            consumer.accept(new IClientItemExtensions() {
+                private GeoItemRenderer<ExtractinatorBlock.Item> renderer;
+
+                @Override
+                public BlockEntityWithoutLevelRenderer getCustomRenderer() {
+                    if (renderer == null) {
+                        this.renderer = new GeoItemRenderer<>(new GeoModel<>() {
+                            @Override
+                            public ResourceLocation getModelResource(ExtractinatorBlock.Item animatable) {
+                                return ExtractinatorBlockModel.MODEL;
+                            }
+
+                            @Override
+                            public ResourceLocation getTextureResource(ExtractinatorBlock.Item animatable) {
+                                return ExtractinatorBlockModel.TEXTURE;
+                            }
+
+                            @Override
+                            public ResourceLocation getAnimationResource(ExtractinatorBlock.Item animatable) {
+                                return null;
+                            }
+                        });
+                    }
+                    return renderer;
+                }
+            });
+        }
+
+        @Override
+        public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {}
+
+        @Override
+        public AnimatableInstanceCache getAnimatableInstanceCache() {
+            return CACHE;
+        }
     }
 }
