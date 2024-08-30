098a503b46912f3ca7abc202626d1497549296ff
voila <voila1106@gmail.com>
2024-08-30 14:28:23 +0800

    肢解蘑菇牛蘑菇（直接不渲染）

---
+++ src/main/java/org/confluence/mod/mixin/client/MooshroomMushroomLayerMixin.java
@@ -0,0 +1,20 @@
+package org.confluence.mod.mixin.client;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.layers.MushroomCowMushroomLayer;
+import net.minecraft.world.entity.animal.MushroomCow;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MushroomCowMushroomLayer.class)
+public abstract class MooshroomMushroomLayerMixin<T extends MushroomCow> {
+    @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/animal/MushroomCow;FFFFFF)V", at = @At("HEAD"),cancellable = true)
+    private void render(PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight, T pLivingEntity, float pLimbSwing, float pLimbSwingAmount, float pPartialTicks, float pAgeInTicks, float pNetHeadYaw, float pHeadPitch, CallbackInfo ci){
+        if(pLivingEntity.isDeadOrDying()){
+            ci.cancel();
+        }
+    }
+}
