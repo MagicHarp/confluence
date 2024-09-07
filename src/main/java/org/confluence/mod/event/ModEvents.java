c2c1fa51e90c560ce18da21aa864b1d2c3257ba4
westernat <westernat233@163.com>
2024-09-07 23:10:11 +0800

    工匠作坊采集

--- src/main/java/org/confluence/mod/event/ModEvents.java
+++ src/main/java/org/confluence/mod/event/ModEvents.java
@@ -1,17 +1,24 @@
 package org.confluence.mod.event;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraftforge.common.crafting.CraftingHelper;
 import net.minecraftforge.event.entity.EntityAttributeModificationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegisterEvent;
 import org.confluence.mod.Confluence;
 import org.confluence.mod.effect.ModEffects;
 import org.confluence.mod.effect.neutral.CerebralMindtrickEffect;
 import org.confluence.mod.misc.ModAttributes;
 import org.confluence.mod.misc.ModConfigs;
 import org.confluence.mod.network.NetworkHandler;
+import org.confluence.mod.recipe.AmountIngredient;
+
+import static org.confluence.mod.Confluence.MODID;
 
 @Mod.EventBusSubscriber(modid = Confluence.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class ModEvents {
@@ -27,6 +34,13 @@ public final class ModEvents {
     public static void loadComplete(FMLLoadCompleteEvent event) {
         event.enqueueWork(() -> {
             ModEffects.CEREBRAL_MINDTRICK.get().addAttributeModifier(ModAttributes.getCriticalChance(), CerebralMindtrickEffect.CRIT_UUID, 0.04, AttributeModifier.Operation.ADDITION);
+        });
+    }
+
+    @SubscribeEvent
+    public static void register(RegisterEvent event) {
+        event.register(ForgeRegistries.Keys.RECIPE_SERIALIZERS, helper -> {
+            CraftingHelper.register(new ResourceLocation(MODID, "amount"), AmountIngredient.Serializer.INSTANCE);
         });
     }
 
