7334c4a6f6bd00eba31edc0f390ba771b3c7135e
westernat <westernat233@163.com>
2024-09-08 17:49:03 +0800

    上下型多方块

---
+++ src/main/java/org/confluence/mod/block/UpperLowerCombineBlock.java
@@ -0,0 +1,51 @@
+package org.confluence.mod.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import org.jetbrains.annotations.Nullable;
+
+import static net.minecraft.world.level.block.state.properties.BlockStateProperties.DOUBLE_BLOCK_HALF;
+
+public abstract class UpperLowerCombineBlock extends Block {
+    public UpperLowerCombineBlock(Properties pProperties) {
+        super(pProperties);
+        registerDefaultState(stateDefinition.any().setValue(DOUBLE_BLOCK_HALF, DoubleBlockHalf.LOWER));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(DOUBLE_BLOCK_HALF);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        Level level = pContext.getLevel();
+        return level.getBlockState(pContext.getClickedPos().above()).canBeReplaced(pContext) ? defaultBlockState() : null;
+    }
+
+    @Override
+    public void setPlacedBy(Level pLevel, BlockPos pPos, BlockState pState, @Nullable LivingEntity pPlacer, ItemStack pStack) {
+        if (!pLevel.isClientSide) {
+            pLevel.setBlockAndUpdate(pPos.above(), defaultBlockState().setValue(DOUBLE_BLOCK_HALF, DoubleBlockHalf.UPPER));
+        }
+    }
+
+    @Override
+    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pMovedByPiston) {
+        super.onRemove(pState, pLevel, pPos, pNewState, pMovedByPiston);
+        pLevel.setBlockAndUpdate(pPos.relative(getConnectedDirection(pState)), Blocks.AIR.defaultBlockState());
+    }
+
+    public static Direction getConnectedDirection(BlockState blockState) {
+        return blockState.getValue(DOUBLE_BLOCK_HALF) == DoubleBlockHalf.LOWER ? Direction.UP : Direction.DOWN;
+    }
+}
