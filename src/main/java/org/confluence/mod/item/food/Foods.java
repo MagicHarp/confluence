d652ec2b22a078a3f922be9c50f8a868ed648990
MagicHarp <161911368+MagicHarp@users.noreply.github.com>
2024-09-16 14:29:44 +0800

    微调

--- src/main/java/org/confluence/mod/item/food/Foods.java
+++ src/main/java/org/confluence/mod/item/food/Foods.java
@@ -124,8 +124,9 @@ public enum Foods implements EnumRegister<Item> {
     //节日特有
     ZONGZI("zongzi", () -> new FireproofFoodItem(FoodType.LOW)),
     HONEY_MOONCAKES("honey_mooncakes", () -> new FireproofFoodItem(FoodType.MEDIUM)),
-    EGG_YOLK_MOONCAKES("egg_yolk_mooncakes", () -> new FireproofFoodItem(FoodType.MEDIUM)),
     HONEY_MOONCAKES_CHUNKS("honey_mooncakes_chunks", () -> new FireproofFoodItem(FoodType.MOONCAKES)),
+    EGG_YOLK_MOONCAKES("egg_yolk_mooncakes", () -> new FireproofFoodItem(FoodType.MEDIUM)),
+
     EGG_YOLK_MOONCAKES_CHUNKS("egg_yolk_mooncakes_chunks", () -> new FireproofFoodItem(FoodType.MEDIUM)),
 
     ;
