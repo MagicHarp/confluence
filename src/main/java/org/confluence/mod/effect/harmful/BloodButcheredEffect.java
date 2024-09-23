832366996f4cd88e2abaf41cd937600402838b63
MagicHarp <@2749380016@qq.com>
2024-09-23 20:37:09 +0800

    屠刀针对于无敌帧改动后的平衡，空岛村宝箱

--- src/main/java/org/confluence/mod/effect/harmful/BloodButcheredEffect.java
+++ src/main/java/org/confluence/mod/effect/harmful/BloodButcheredEffect.java
@@ -12,7 +12,27 @@ public class BloodButcheredEffect extends MobEffect {
 
     @Override
     public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
-        pLivingEntity.hurt(pLivingEntity.level().damageSources().magic(),  pAmplifier + 0.1F);
+        float damage;
+
+        switch (pAmplifier) {
+            case 2:
+                damage = 0.1F;
+                break;
+            case 3:
+                damage = 0.15F;
+                break;
+            case 4:
+                damage = 0.2F;
+                break;
+            case 5:
+                damage = 0.25F;
+                break;
+            default:
+                damage = 0.05F;
+                break;
+        }
+
+        pLivingEntity.hurt(pLivingEntity.level().damageSources().magic(), damage);
     }
 
     @Override
