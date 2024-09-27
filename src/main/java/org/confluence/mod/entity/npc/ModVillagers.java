3501bf8323a6244415818e994c411906cc17b9f7
MagicHarp <@2749380016@qq.com>
2024-09-27 14:42:12 +0800

    暂时码完了农民的

--- src/main/java/org/confluence/mod/entity/npc/ModVillagers.java
+++ src/main/java/org/confluence/mod/entity/npc/ModVillagers.java
@@ -24,8 +24,10 @@ import org.confluence.mod.block.ModBlocks;
 import org.confluence.mod.block.common.DecorativeBlocks;
 import org.confluence.mod.item.ModItems;
 import org.confluence.mod.item.common.Materials;
+import org.confluence.mod.item.food.Foods;
 
 import java.util.List;
+import java.util.Random;
 
 import static org.confluence.mod.Confluence.MODID;
 
@@ -51,18 +53,31 @@ public final class ModVillagers {
     public static void villagerTrades(VillagerTradesEvent event) {
         VillagerProfession type = event.getType();
         Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+        Random random = new Random();
         if (type == SKY_MILLER.get()) {
-            trades.get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD), new ItemStack(ModBlocks.CLOUD_BLOCK.get(), 2), 12, 5, 0.05f));
+            trades.get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD), new ItemStack(ModBlocks.CLOUD_BLOCK.get(), getRandomQuantity(2, 3)),12, 5, 0.05f));
             trades.get(1).add(new BasicItemListing(new ItemStack(Materials.WEAVING_CLOUD_COTTON.get(), 5), new ItemStack(Items.EMERALD, 1), 12, 5, 0.05f));
-            trades.get(2).add(new BasicItemListing(new ItemStack(Items.DIRT, 64), new ItemStack(Items.EMERALD, 1), 2, 10, 0.05f));
+            trades.get(2).add(new BasicItemListing(new ItemStack(Items.DIRT, getRandomQuantity(61, 64)),new ItemStack(Items.EMERALD, 1), 2, 10, 0.05f));
             trades.get(3).add(new BasicItemListing(new ItemStack(Materials.EMERALD_COIN.get(), 1), new ItemStack(ModItems.CLOUDWEAVER_SEED.get(), 3), 25, 10, 0.05f));
             trades.get(3).add(new BasicItemListing(new ItemStack(Items.WATER_BUCKET, 1), new ItemStack(ModBlocks.RAIN_CLOUD_BLOCK.get(), 1), 12, 20, 0.05f));
-            trades.get(3).add(new BasicItemListing(new ItemStack(Materials.FALLING_STAR.get(), 20), new ItemStack(DecorativeBlocks.SUN_PLATE.get(), 10), 12, 20, 0.05f));
+            trades.get(3).add(new BasicItemListing(new ItemStack(Materials.FALLING_STAR.get(), getRandomQuantity(20, 22)), new ItemStack(DecorativeBlocks.SUN_PLATE.get(), 10), 12, 20, 0.05f));
             trades.get(4).add(new BasicItemListing(new ItemStack(Items.POWDER_SNOW_BUCKET, 1), new ItemStack(Items.EMERALD, 1), 12, 30, 0.05f));
             trades.get(5).add(new BasicItemListing(new ItemStack(Materials.EMERALD_COIN.get(), 2), new ItemStack(ModBlocks.SKY_MILL.get(), 1), 12, 30, 0.05f));
         } else if (type == VillagerProfession.FARMER) {
-            trades.get(1).add(new SkyVillagerItemListing(new ItemStack(ModBlocks.FLOATING_WHEAT.get(), 5), new ItemStack(Items.EMERALD), 12, 25, 0.05F));
+            trades.get(1).add(new SkyVillagerItemListing(new ItemStack(ModBlocks.FLOATING_WHEAT.get(), getRandomQuantity(4, 8)), new ItemStack(Items.EMERALD), 5, 25, 0.05F));
+            trades.get(1).add(new SkyVillagerItemListing(new ItemStack(Materials.WEAVING_CLOUD_COTTON.get(),getRandomQuantity(4, 8)), new ItemStack(Items.EMERALD), 5, 25, 0.05F));
+            trades.get(1).add(new SkyVillagerItemListing(new ItemStack(Items.EMERALD,3), new ItemStack(Foods.CLOUD_BREAD.get()), 5, 25, 0.05F));
+            trades.get(2).add(new SkyVillagerItemListing(new ItemStack(Items.EMERALD,2), new ItemStack(Foods.APRICOT.get()), 10, 25, 0.05F));
+            trades.get(2).add(new SkyVillagerItemListing(new ItemStack(Items.EMERALD,2), new ItemStack(Foods.PLUM.get()), 10, 25, 0.05F));
+            trades.get(2).add(new SkyVillagerItemListing(new ItemStack(Items.EMERALD,6), new ItemStack(Foods.HONEY_MOONCAKES.get(),1), 10, 25, 0.05F));
+            trades.get(3).add(new SkyVillagerItemListing(new ItemStack(Materials.FALLING_STAR.get(),getRandomQuantity(20, 22)), new ItemStack(Items.EMERALD,2), 10, 25, 0.05F));
+            trades.get(4).add(new SkyVillagerItemListing(new ItemStack(Items.EMERALD,1), new ItemStack(ModItems.CLOUDWEAVER_SEED.get(),2), 10, 25, 0.05F));
+            trades.get(4).add(new SkyVillagerItemListing(new ItemStack(Items.EMERALD,1), new ItemStack(ModItems.STELLAR_BLOSSOM_SEED.get(),2), 10, 25, 0.05F));
+            trades.get(4).add(new SkyVillagerItemListing(new ItemStack(Items.EMERALD,1), new ItemStack(ModItems.FLOATING_WHEAT_SEED.get(),2), 10, 25, 0.05F));
         }
+    }
+    private static int getRandomQuantity(int min, int max) {
+        return min + new Random().nextInt(max - min + 1); // Generates a number between min and max inclusive
     }
 
     public static void setVillagerType(Villager villager) {
