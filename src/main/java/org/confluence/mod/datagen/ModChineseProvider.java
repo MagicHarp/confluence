65c7331a3aaec406e18afc36096db407c67cfe3b
MagicHarp <161911368+MagicHarp@users.noreply.github.com>
2024-10-03 15:47:38 +0800

    修复天磨

--- src/main/java/org/confluence/mod/datagen/ModChineseProvider.java
+++ src/main/java/org/confluence/mod/datagen/ModChineseProvider.java
@@ -488,6 +488,7 @@ public class ModChineseProvider extends LanguageProvider {
         add("title.confluence.shimmer_transmutation", "微光嬗变");
         add("title.confluence.altar", "献祭");
         add("title.confluence.sky_mill", "天域工艺");
+        add("container.confluence.sky_mill", "天域工艺");
         add("condition.confluence.shimmer_transmutation", "需要的游戏阶段: %s");
 
         add("item.confluence.aglet.info", "它可以在地表宝箱、木匣和珍珠木匣中找到。");
