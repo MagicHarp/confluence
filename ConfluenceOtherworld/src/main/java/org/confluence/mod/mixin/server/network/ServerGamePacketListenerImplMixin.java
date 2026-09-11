5d3cf72f6a2bd314e3cea8b40a2078677633d498
westernat <westernat233@163.com>
2026-09-11 20:52:44 +0800

    修复与Bigger Stacks的Mixin冲突

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/mixin/server/network/ServerGamePacketListenerImplMixin.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/mixin/server/network/ServerGamePacketListenerImplMixin.java
@@ -1,22 +1,29 @@
 package org.confluence.mod.mixin.server.network;
 
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.world.item.ItemStack;
 import org.confluence.lib.util.LibUtils;
 import org.confluence.mod.mixed.IServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(ServerGamePacketListenerImpl.class)
 public abstract class ServerGamePacketListenerImplMixin {
     /// 允许创造模式同步使用全局扩展堆叠上限的物品栈。
-    @ModifyConstant(method = "handleSetCreativeModeSlot", constant = @Constant(intValue = 64))
-    private int extendCreativeStackLimit(int original) {
-        return LibUtils.getMaxStackSize(original);
+    @Definition(id = "itemstack", local = @Local(type = ItemStack.class))
+    @Definition(id = "getCount", method = "Lnet/minecraft/world/item/ItemStack;getCount()I")
+    @Expression("itemstack.getCount() <= ?")
+    @ModifyExpressionValue(method = "handleSetCreativeModeSlot", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private boolean extendCreativeStackLimit(boolean original, @Local ItemStack itemstack) {
+        if (original) return true;
+        return itemstack.getCount() <= LibUtils.getMaxStackSize(64);
     }
 
     @WrapOperation(method = {"handleMoveVehicle", "handleMovePlayer"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;checkMovementStatistics(DDD)V"))
